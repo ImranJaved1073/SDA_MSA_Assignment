d454136ea9f3c8e4b2f758a77bd8fe9e2ad7cbb7
Imran Javed <bsef21m033@pucit.edu.pk>
2024-09-18 20:43:53 +0500

    Add project files.

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SDA_MSA_Assignment.Models;
+
+namespace SDA_MSA_Assignment.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int productId);
+        void InsertProduct(Product product);
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
